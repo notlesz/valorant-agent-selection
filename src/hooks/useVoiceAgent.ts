d5448c7338327eedccb65f4130b559572bb533be
Elton Souza <elton.souza1515@gmail.com>
2022-06-13 16:09:18 -0300

    create function pause agent voice

--- src/hooks/useVoiceAgent.ts
+++ src/hooks/useVoiceAgent.ts
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 
 export const useVoiceAgent = () => {
   const [voice, setVoice] = useState<string>();
-  const [isPlayed, setIsPlayed] = useState<boolean>(false);
 
   const audio = new Audio(voice);
   audio.volume = 1;
 
   const play = () => audio.play();
+  const pause = () => audio.pause();
 
   useEffect(() => {
     play();
   }, [voice]);
   return {
     setVoice,
+    pause
   };
 };
