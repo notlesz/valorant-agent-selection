b25fc2a613552796c267a01794f4e8791eba2276
Elton Souza <elton.souza1515@gmail.com>
2022-06-13 16:09:48 -0300

    add function pause agent voice

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,71 @@
 import { FormEvent, useEffect, useState } from "react";
 import { Agent, AgentInfo, AgentAbility } from "./@types/agents";
-import AgentInfoSelect from "./components/AgentInfoSelect";
+import AgentList from "./components/AgentList";
+import SelectedAgentInfo from "./components/SelectedAgentInfo";
 import { useVoiceAgent } from "./hooks/useVoiceAgent";
 import { getListAgents } from "./services/api";
-import { languages } from './util/languages'
+import { languages } from "./util/languages";
+
 function App() {
-  const [listAgents, setListAgents] = useState<Agent[]>([]);
+  const [agentList, setAgentList] = useState<Agent[]>([]);
   const [agentView, setAgentView] = useState<Agent>();
   const [agentInfo, setAgentInfo] = useState<AgentInfo>();
   const [agentAbility, setAgentAbility] = useState<AgentAbility>();
   const [showAgentInfo, setShowAgentInfo] = useState<boolean>(false);
   const [showAgentAbility, setShowAgentAbility] = useState<boolean>(false);
-  const [language, setLanguage] = useState('pt-BR')
+  const [language, setLanguage] = useState("pt-BR");
 
-  const { setVoice } = useVoiceAgent();
+  const { setVoice, pause } = useVoiceAgent();
 
-  const getAgents = async (code: string = 'pt-BR') => {
+  const getAgents = async (code: string = "pt-BR") => {
     const { data } = await getListAgents(code);
-    setListAgents(data.data);
+    setAgentList(data.data);
   };
 
-  const playVoiceAgent = (audio: string) => {
+  const handleAgentAudio = (audio: string | undefined) => {
+    pause();
     setVoice(audio);
+  };
+
+  const handleAgentView = (agent: Agent) => {
+    setAgentView(agent);
+  };
+
+  const handleAgentInfo = (agent: AgentInfo) => {
+    setAgentInfo(agent);
   };
 
   const handleShowAgentInfo = () => {
     setShowAgentInfo(true);
     setShowAgentAbility(false);
-    setAgentAbility(undefined)
+    setAgentAbility(undefined);
   };
 
   const handleShowAgentAbility = () => {
     setShowAgentInfo(false);
     setShowAgentAbility(true);
-    setAgentInfo(undefined)
+    setAgentInfo(undefined);
   };
 
-  const handleChangelanguage = (option: string) => {
-    setLanguage(option)
-  }
+  const handleLanguageChange = (option: string) => {
+    setLanguage(option);
+  };
 
   useEffect(() => {
     getAgents();
   }, []);
 
   useEffect(() => {
-    setAgentView(undefined)
-    getAgents(language)
-  }, [language])
+    handleAgentAudio(undefined)
+    setAgentView(undefined);
+    getAgents(language);
+  }, [language]);
 
   return (
     <main
-      className={` flex flex-col items-center ${agentView ? "justify-between" : "justify-end"
-        } h-[100vh] overflow-hidden max-w-[1600px] w-full  ml-auto mr-auto m-0 `}
+      className={` flex flex-col items-center ${
+        agentView ? "justify-between" : "justify-end"
+      } h-[100vh] overflow-hidden max-w-[1600px] w-full  ml-auto mr-auto m-0 `}
     >
       {agentView ? (
         <div>
@@ -71,14 +84,16 @@ function App() {
                 {agentView.displayName}
               </span>
             </div>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <div className="flex gap-[16px]">
                 <div
                   onClick={() => {
                     handleShowAgentInfo();
                     setAgentInfo(agentView);
                   }}
-                  className={`2xl:w-[40%] 2xl:h-[60px] flex justify-center items-center border-solid border-[1px] border-transparent hover:bg-select rounded ${agentView === agentInfo ? 'bg-select' : '' }`}
+                  className={`2xl:w-[40%] 2xl:h-[60px] flex justify-center items-center border-solid border-[1px] border-transparent hover:bg-select rounded ${
+                    agentView === agentInfo ? "bg-select" : ""
+                  }`}
                 >
                   <img
                     src={agentView.role.displayIcon}
@@ -94,58 +109,62 @@ function App() {
                           handleShowAgentAbility();
                           setAgentAbility(ability);
                         }}
-                        className={`2xl:w-[40%] 2xl:h-[60px] flex justify-center items-center hover:bg-select border-solid border-[1px] border-transparent rounded ${agentAbility === ability ? 'bg-select' : ''}`}
+                        className={`cursor-pointer 2xl:w-[40%] 2xl:h-[60px] flex justify-center items-center hover:bg-select border-solid border-[1px] border-transparent rounded ${
+                          agentAbility === ability ? "bg-select" : ""
+                        }`}
                       >
                         <img
                           src={ability.displayIcon}
                           alt={ability.displayName}
-                          className="w-[30px] h-[30px] 2xl:w-[40px] 2xl:h-[40px] cursor-pointer"
+                          className="w-[30px] h-[30px] 2xl:w-[40px] 2xl:h-[40px]"
                         />
                       </div>
                     )
                 )}
               </div>
-              <AgentInfoSelect agentAbility={agentAbility} agentInfo={agentInfo} showAbility={showAgentAbility} showInfo={showAgentInfo} />
+              <SelectedAgentInfo
+                agentAbility={agentAbility}
+                agentInfo={agentInfo}
+                showAbility={showAgentAbility}
+                showInfo={showAgentInfo}
+              />
             </div>
           </div>
         </div>
       ) : (
         <h1 className="font-anton text-[50px] text-primary uppercase absolute top-[40%]">
-        {languages.map( (languageSelect) => {
-          if(languageSelect.code === language) return languageSelect.textTranslate 
-        })}
+          {languages.map((languageSelect) => {
+            if (languageSelect.code === language)
+              return languageSelect.textTranslate;
+          })}
         </h1>
       )}
 
-      <div className="flex flex-wrap gap-[5px] lg:w-[800px] 2xl:w-[1100px] absolute bottom-2">
-        {listAgents.length > 0 && (
-          <>
-            {listAgents.map((agent) => (
-              <div
-                onClick={() => {
-                  playVoiceAgent(agent.voiceLine.mediaList[0].wave);
-                  setAgentView(agent);
-                  setAgentInfo(agent);
-                  handleShowAgentInfo();
-                }}
-                className={`border-solid border-[1px] border-zinc-400 hover:bg-select ${agent === agentView ? 'bg-select' : ''}`}
-              >
-                <img
-                  src={agent.displayIcon}
-                  alt={agent.displayName}
-                  className="2xl:w-[100px] 2xl:h-[100px] lg:w-[70px] lg:h-[70px] cursor-pointer"
-                />
-              </div>
-            ))}
-          </>
-        )}
-      </div>
+      <AgentList
+        agentList={agentList}
+        agentView={agentView}
+        handleAgentInfo={handleAgentInfo}
+        handleAgentAudio={handleAgentAudio}
+        handleAgentView={handleAgentView}
+        handleShowAgentInfo={handleShowAgentInfo}
+      />
 
-      <div className='absolute right-[70%] lg:top-[20px] 2xl:right-[80%] 2xl:top-[30px]'>
-        <label htmlFor="language" className="font-anton text-primary text-[23px]">Language </label>
-        <select value={language} onChange={(option) => handleChangelanguage(option.target.value)} name="language" id="language" className="bg-black border-solid border-[1px] border-primary font-anton px-[10px] text-primary">
+      <div className="absolute right-[70%] lg:top-[20px] 2xl:right-[80%] 2xl:top-[30px]">
+        <label
+          htmlFor="language"
+          className="font-anton text-primary text-[23px]"
+        >
+          Language{" "}
+        </label>
+        <select
+          value={language}
+          onChange={(option) => handleLanguageChange(option.target.value)}
+          name="language"
+          id="language"
+          className="bg-black border-solid border-[1px] border-primary font-anton px-[10px] text-primary"
+        >
           {languages.map((language, id) => (
-            <option  key={id} value={language.code}>
+            <option key={id} value={language.code}>
               {language.name}
             </option>
           ))}
